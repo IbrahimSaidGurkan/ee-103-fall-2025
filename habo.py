c3818241d207902828fa0b99c0845876ddf0163b
ibrahim said gürkan <habolumango@gmail.com>
2025-10-22 19:29:43 +0300

    değ

--- habo.py
+++ habo.py
@@ -4,3 +4,17 @@ print(s)
 s='b'+s[1:3]
 print(s)
 '''
+
+'''
+orm=input("ormandasin saga mi gitmek istersin sola mi?")
+a=0
+while orm=="sag":
+    a=a+1
+    print("dostum yanlis yon ",a,".defa yanlis yone gidiyon")
+    if(a==3):
+        print(a,". defa yanlis yone donuyon malsin")
+    orm=input("ormandasin saga mi gitmek istersin sola mi?2")
+print("helal olsun")    
+'''
+
+
